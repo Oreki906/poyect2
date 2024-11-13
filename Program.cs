22c61f3d01208dc1480d0edde35d6aa4c898776b
flore <flore@LAPTOP-92DN0AH1>
2024-11-13 10:32:59 -0800

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace burbujasprueba
+{
+    internal class Program
+    {
+        static int[] numeros = new int[] { 10, 2, 3, 4, 5, 1, 9 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inicio");
+            mostar();
+            for(int i =0; i < numeros.Length; i++)
+            {
+
+            }
+        }
+        public static void mostar()//unicamente muestra el resultado
+        {
+            foreach (var numeros in numeros)
+            {
+                Console.WriteLine(numeros + ",");
+            }
+            Console.WriteLine("\n");
+            
+        }
+    }
+}
